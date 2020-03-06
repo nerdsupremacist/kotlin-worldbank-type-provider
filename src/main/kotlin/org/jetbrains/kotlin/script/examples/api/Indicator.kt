23b52f3866f110c1202b5979edea9152d91ca509
Mathias Quintero <me@quintero.io>
2020-03-06 17:12:07 +0100

    Ignoring unknown properties in data from indicators

--- src/main/kotlin/org/jetbrains/kotlin/script/examples/api/Indicator.kt
+++ src/main/kotlin/org/jetbrains/kotlin/script/examples/api/Indicator.kt
@@ -1,11 +1,14 @@
 package org.jetbrains.kotlin.script.examples.api
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
 class Indicator internal constructor(
     val id: String,
     val name: String,
     private val countryOrRegionCode: String,
     private val client: WorldBankClient
 ) {
+    @JsonIgnoreProperties(ignoreUnknown = true)
     private class Data(val date: String, val value: String?)
 
     suspend operator fun invoke(): List<Pair<Int, Double>> = client
